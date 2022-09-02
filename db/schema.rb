c6cb8f06d5f6952e22e6b083effeac0701302b5e
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-03 00:40:01 +0700

    update validator

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_163913) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_172255) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "modifed_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
