3661241a2cc422163a006885e998077de03d2259
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-03 22:56:34 +0700

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 	get "about", to: "pages#about"
 	
   resources :articles
+
+	get 'signup', to: 'users#new'
+	post 'users', to: 'users#create'
+
+	resources :users, except: [:new]
   # Defines the root path route ("/")
   # root "articles#index"
 end
